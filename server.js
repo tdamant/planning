f658aa08e3b68393d4b56e1ed2c12a0579fe33b4
Tom Damant <tom@gluon.local>
2019-03-16 12:33:51 +0000

    add skeleton sign up page

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
     res.send("Continous deployment!");
 });
 
+app.get("/sign-up", (req, res) => {
+    res.send("Please sign up!")
+});
+
 app.listen(port, () => console.log(`Planning app listening here: ${port}!`));
