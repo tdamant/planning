ba6ccf8380e2122582394aa277bdee0a09e709ac
Szilvi Horvath <szil.horvath@gmail.com>
2019-03-18 22:30:28 +0000

    adjust newTripInterface and db set up instructions in readme to only take trip name and description

--- views/newTripInterface.js
+++ views/newTripInterface.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
         event.preventDefault();
         let tripName = $('#tripName').val();
         let description = $('#description').val();
-        let attendee1 = $('#attendee1').val();
-        let tripStart = $('#tripStart').val();
-        let tripEnd = $('#tripEnd').val();
-        $.post("/trips/create", {tripName: tripName, description: description, attendee1: attendee1, tripStart: tripStart, tripEnd: tripEnd});
+        $.post("/trips/create", {tripName: tripName, description: description});
         $(location).attr('href', `/trip?${tripName}`)
     });
 });
