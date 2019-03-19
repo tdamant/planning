f83fa7c208e54010f45baf150a92b30e2ed2c727
Szilvi Horvath <szil.horvath@gmail.com>
2019-03-19 10:53:23 +0000

    add stages form

---
+++ features/newStage.test.js
@@ -0,0 +1,19 @@
+describe('New Stage', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:5000/new-trip');
+        await expect(page).toFillForm('form[name="addTrip"]', {
+            tripName: 'Trip name',
+            description: 'Trip description'
+          });
+        await page.click('#submit');
+        await page.waitForNavigation({'waitUntil': 'networkidle0'});
+    });
+
+    it('can submit form, load relevant trip page with name and description', async () => {
+      await expect(page).toFillForm('form[name="addStage"]', {
+          stageName: 'book flight',
+          stageContent: 'Unique stage description',
+          stageDueDate: '29/03/2019'
+      });
+  });
+});
