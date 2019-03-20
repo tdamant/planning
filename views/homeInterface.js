3b3ae4999b99cfa945ec51220104daa1be1816a3
Szilvi Horvath <szil.horvath@gmail.com>
2019-03-20 16:05:50 +0000

    log in and sign up hooked up to the main frame

--- views/homeInterface.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    const response = await fetch("/example");
-    const data = await response.json();
-    console.log(data);
-});
--- views/loginInterface.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var urlParams = new URLSearchParams(location.search);
-    let fromUrl = urlParams.get('fromUrl');
-    if (fromUrl) {
-        $('#logIn').append('<input type="hidden" name="fromUrl" value="' + fromUrl + '">')
-    }
-});
