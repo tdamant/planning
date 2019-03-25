05638a1e526f6273da28824826b93ba438aa99b7
isabelcooper <isabel@triptease.com>
2019-03-25 14:29:37 +0000

    your trips showing on home page

--- models/lib/users.js
+++ models/lib/users.js
@@ -1,4 +1,5 @@
 const connection = require("../../database/connection");
+const Trip = require("./trips.js");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -37,6 +38,35 @@ class User {
       return results.rows
   };
 
+   static async asyncForEach(array, callback) {
+      for (let index = 0; index < array.length; index++) {
+          await callback(array[index], index, array);
+      }
+  };
+
+  static async getTripsByUser(userId) {
+      let matchingTripIds = await connection.pool.query(`SELECT trip_id FROM trips_users WHERE user_id = ${userId}`);
+      let matchingTrips = await matchingTripIds.rows;
+      let tripsArray =[];
+      await this.asyncForEach(matchingTrips, async(trip) => {
+              let fullTrip = await Trip.getById(trip.trip_id);
+              tripsArray.push(fullTrip);
+      });
+      return tripsArray;
+  }
+
 }
 
 module.exports = User;
+
+//
+// async function asyncForEach(array, callback) {
+//     for (let index = 0; index < array.length; index++) {
+//         await callback(array[index], index, array);
+//     }
+// };
+// await asyncForEach(stages, async(stage) => {
+//     let textArray = await ReminderFinder.getUsersOnStage(stage);
+//     // textsToSend = [ ... textArray];
+//     textsToSend.push(textArray);
+// });
