77ae6d12a38f8629ed0cd4b9d907a045bfd151dd
isabelcooper <isabel@triptease.com>
2019-03-26 16:35:03 +0000

    only shoq important checkbox to organiser

--- views/js/comments.js
+++ views/js/comments.js
@@ -1,5 +1,13 @@
 $(document).ready(async function() {
-    const loadComments = async() =>{
+    $('#commentButton').on("click", function(event) {
+        event.preventDefault();
+        let comment = $("#comment").val();
+        let announcement = $("#announcement").is(':checked');
+        $.post("/comments", {comment: comment, announcement: announcement});
+        location.reload()
+    });
+
+    const loadCommentData = async() =>{
         let commentsResponse = await fetch("/comments");
         let comments =  await commentsResponse.json();
         let importantComments = await comments.filter(comment=> { return comment.announcement === true });
@@ -10,30 +18,47 @@ $(document).ready(async function() {
         };
     };
 
-    let comments = await loadComments();
-
-    async function asyncForEach(array, callback) {
-        for (let index = 0; index < array.length; index++) {
-            await callback(array[index], index, array);
-        }
+    const checkIfOrganiser = async() => {
+        let userId = await $.get("/whoami");
+        let tripId = location.search.substr(4);
+        let tripResponse = await fetch(`/trips/${tripId}`);
+        let trip = await tripResponse.json();
+        let tripOrganiserId = trip.organiser;
+        return userId == tripOrganiserId
     };
 
-    await asyncForEach(comments.other, async(comment) => {
-        let userData = await $.get(`/users/${comment.user_id}`);
-        $("#comment-list").append(`${comment.comment} --- ${userData.first_name}<br>`)
-    });
+    const showImportantCheckbox = async () => {
+        console.log(isOrganiser);
+        if(isOrganiser === false) {
+            $("#announcement").hide();
+            $("#announcement-label").hide();
+        }
 
-    await asyncForEach(comments.important, async(comment) => {
-        let userData = await $.get(`/users/${comment.user_id}`);
-        $("#important-comment-list").append(`${comment.comment} --- ${userData.first_name}<br>`)
-    });
+    };
 
-    $('#commentButton').on("click", function(event) {
-        event.preventDefault();
-        let comment = $("#comment").val();
-        let announcement = $("#announcement").is(':checked');
-        $.post("/comments", {comment: comment, announcement: announcement});
-        location.reload()
-    });
+    const formatComments = async() => {
+        async function asyncForEach(array, callback) {
+            for (let index = 0; index < array.length; index++) {
+                await callback(array[index], index, array);
+            }
+        };
+
+        await asyncForEach(comments.other, async(comment) => {
+            let userData = await $.get(`/users/${comment.user_id}`);
+            $("#comment-list").append(`${comment.comment} --- ${userData.first_name}<br>`)
+        });
+
+        await asyncForEach(comments.important, async(comment) => {
+            let userData = await $.get(`/users/${comment.user_id}`);
+            $("#important-comment-list").append(`${comment.comment} --- ${userData.first_name}<br>`)
+        });
+    };
+
+
+    let comments = await loadCommentData();
+    let isOrganiser = await checkIfOrganiser();
+    showImportantCheckbox();
+    formatComments();
+
 });
 
