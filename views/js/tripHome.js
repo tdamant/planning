9eb3b7e5383cc40b588b88327dc5ea2fae605118
Chloe <chloemontgomery01@gmail.com>
2019-03-26 12:32:50 +0000

    can only vote once

--- views/js/tripHome.js
+++ views/js/tripHome.js
@@ -12,50 +12,73 @@ $(document).ready(async () => {
     let votes = await $.get("/polls/votes", {tripId: tripId});
     return {
       tripId: tripId,
-        userId: userId,
-        pollsData: pollsData,
-        votes: votes
+      userId: userId,
+      pollsData: pollsData,
+      votes: votes
     }
   };
 
   const buildPoll = (poll) => {
       let pollDiv = "";
       const addOptions = (options) => {
-          options.forEach((option, index) => {
-            let votes = getVotes(`${index}-poll${poll.id}`);
-            console.log(votes);
-            pollDiv += `<input id = "${index}-poll${poll.id}" type="checkbox"><label for ="${option}"> ${option} -- VOTES SO FAR -- ${votes} </label>`
-          });
+        options.forEach((option, index) => {
+          let votes = getVotes(`${index}-poll${poll.id}`);
+          pollDiv += `<input id = "${index}-poll${poll.id}" type="checkbox"><label for ="${option}"> <span id="option">${option} -- </span> <span id="votes"> VOTES SO FAR -- ${votes}</span> </label>`
+        });
       };
       const addDivId = (type) => {
-          pollDiv += `<div id="${type}"><form id="votesFor${type}"><fieldset><legend>${type}</legend>`
+        pollDiv += `<div id="${type}"><form id="votesFor${type}"><fieldset><legend>${type}</legend>`
       };
       let options = poll.options.split(",");
       addDivId(poll.type);
       addOptions(options);
       pollDiv += `<input id="${poll.type}-submit" type="submit"> </fieldset></form> </div>`;
       $("#pollsContainer").append(pollDiv);
+
       $(`#${poll.type}-submit`).on("click", (event) => {
           event.preventDefault();
           let votes = [];
           $(`#votesFor${poll.type} input[type=checkbox]`).each( function (index) {
-              if ($(`#${index}-poll${poll.id}`).prop("checked")) {
-                  votes.push(`${index}-poll${poll.id}`)
-              }
+            if ($(`#${index}-poll${poll.id}`).prop("checked")) {
+              votes.push(`${index}-poll${poll.id}`)
+            };
           });
-          saveVotes(data.tripId, poll.id, data.userId, votes.join(','))
+        saveVotes(data.tripId, poll.id, data.userId, votes.join(','))
+        location.reload()
       });
   };
 
+  const buildPollResults = (poll) => {
+    let resultsDiv = `<div id"${poll.type} > <p id"thanks"> Thanks for voting on ${poll.type.toLowerCase()}! </p> `
+    const getOptions = (options) => {
+      options.forEach((option, index) => {
+        let votes = getVotes(`${index}-poll${poll.id}`);
+        resultsDiv += `<p>${option} -- VOTES SO FAR -- ${votes}</p><br>`
+      });
+    };
+    let options = poll.options.split(",");
+    getOptions(options);
+    $("#pollsContainer").append(resultsDiv);
+  }
+
   const saveVotes = (tripId, pollId, userId, optionIds) => {
-      console.log(optionIds);
       $.post("/polls/saveVotes", {tripId: tripId, pollId: pollId, userId: userId, optionIds: optionIds})
   };
 
-
-  const addPolls = (polls) => {
+  const addPolls = async (polls) => {
     polls.forEach((poll) => {
+      let pollVotes =
+      data.votes.filter(function(vote) {
+        return vote.poll_id === poll.id
+      })
+      let usersPollVotes =
+      pollVotes.filter(function(vote) {
+        return vote.user_id.toString() === data.userId.toString()
+      })
+      if (usersPollVotes.length === 0)
         buildPoll(poll)
+      else
+        buildPollResults(poll)
     });
   };
 
@@ -65,18 +88,10 @@ $(document).ready(async () => {
       return vote.option_id === optionId
     });
     return relevantVotes.length;
-  }
+  };
 
   let data = await getData();
   addPolls(data.pollsData);
-  console.log(data.votes);
 
-  $('#addStage').on("click", function() {
-    $('#stageCreator').show("fast");
-  });
-
-  $('#saveStage').on("click", function() {
-    $('#stageCreator').hide("fast");
-  });
 
 });
