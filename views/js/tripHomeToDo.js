ab954d3d6b2e0b4721ebec4cce8ac08661944352
Chloe <chloemontgomery01@gmail.com>
2019-03-27 06:31:52 +0000

    add stage deadline now date format

--- views/js/tripHomeToDo.js
+++ views/js/tripHomeToDo.js
@@ -1,29 +1,24 @@
+const showStages = (data) => {
+    const checkUserCompleted = (stage) => {
+      let allResponses = data.votes.filter(function(vote) {
+        return vote.stage_id === stage.id
+      })
+      let userCompleted = allResponses.filter(function(response) {
+        return response.user_id.toString() === data.userId.toString()
+      })
+      return userCompleted.length > 0;
+    };
 
-        //<p id="stages-list" ></p> <br>
-
-// $(document).ready(async () => {
-//
-//   const getUrlParams = (name) => {
-//       var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-//       return results[1];
-//   };
-//
-//   const getData = async () => {
-//     let tripId = await getUrlParams('id');
-//     let userId = await $.get("/whoami");
-//     let pollsData = await $.get("/polls/getPolls", {tripId: tripId });
-//     let votes = await $.get("/polls/votes", {tripId: tripId});
-//     return {
-//       tripId: tripId,
-//       userId: userId,
-//       pollsData: pollsData,
-//       votes: votes
-//     }
-//   };
-//
-//
-//   let data = await getData();
-//   addPolls(data.pollsData);
-//
-//
-// });
+    let stages = data.stages;
+    if(stages.length > 0) {
+        stages.forEach(stage => {
+          let cleanStageDescription = cleanDbString(stage.content);
+          if (checkUserCompleted(stage)) {
+            $('#stages-list').append(`${stage.name} - ${cleanStageDescription} <br>`)
+          }
+          else {
+            $('#stages-list').append(`<s>${stage.name} - ${cleanStageDescription} </s><br>`)
+          }
+        })
+    }
+};
