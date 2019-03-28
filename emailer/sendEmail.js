ba4f6a38323cce825c5e6055ba15956e3c202d2c
Chloe <chloemontgomery01@gmail.com>
2019-03-28 15:12:19 +0000

    emailer working?

--- emailer/sendEmail.js
+++ emailer/sendEmail.js
@@ -13,11 +13,12 @@ class SendMail {
   }
 
   setMailOptions(to, tripid, organiser) {
+    let upper = organiser.charAt(0).toUpperCase() + organiser.substring(1);
     this.mailOptions = {
       from: 'wing.it.hq@gmail.com',
       to: to,
       subject: 'Come fly with me! ;)',
-      html: `<h3>Hi there,</h3><p>${organiser.charAt(0).toUpperCase()} has invited you to an event on Wing.
+      html: `<h3>Hi there,</h3><p>${upper} has invited you to an event on Wing.
       <br>Check it out at: http://stormy-escarpment-39913.herokuapp.com/trip_home?id=${tripid}<p><br>
       <p><i>Wing Team</i></p>`
     }
